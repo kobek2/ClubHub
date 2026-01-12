599cdfd47d999fadac40864515f3d430960b4394
kobethou06@gmail.com <kobethou06@gmail.com>
2026-01-11 22:59:14 -0600

    fix firebase

---
+++ club-task-board/src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCb64M8yhXokADiTh7lA62jaSvxBy4G7pQ",
+  authDomain: "clubhub-e810b.firebaseapp.com",
+  databaseURL: "https://clubhub-e810b-default-rtdb.firebaseio.com",
+  projectId: "clubhub-e810b",
+  storageBucket: "clubhub-e810b.firebasestorage.app",
+  messagingSenderId: "786777391504",
+  appId: "1:786777391504:web:e62ec3e2bec89a19813315"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore (The Database)
+export const db = getFirestore(app);
